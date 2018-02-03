a8026dbea316e56ff5c443aa631116940c3f69bc
Fabian Flores <ffflores1@outlook.com>
2018-02-03 17:40:36 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Bootstrap-Portfolio
+
 ####Fabian Flores
+
 ##Description
 
 This project will create a portfolio layout using the Bootstrap CSS Framework. 
---
+++ init_structure.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+touch index.html contact.html portfolio.html
+#mkdir -vp assets
+#mkdir -vp assets/css
+#mkdir -vp assets/images
+#touch assets/css/style.css
+#touch assets/css/reset.css
+#touch assets/images/example.jpg
